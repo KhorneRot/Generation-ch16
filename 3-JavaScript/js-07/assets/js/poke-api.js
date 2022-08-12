f209c3cfcb58a9282323337fa1dabb1995de8017
KhorneRot <ale_rm96@outlook.com>
2022-08-12 17:01:33 -0500

    npm y pokeapi

--- 3-JavaScript/js-07/assets/js/poke-api.js
+++ 3-JavaScript/js-07/assets/js/poke-api.js
@@ -1,37 +1,83 @@
 /* URL o consumir */
-const urlPokemon = "https://pokeapi.co/api/v2/pokemon/155"
-
-
-
+const urlPokemon = "https://pokeapi.co/api/v2/pokemon/";
 
 /* Elementos del DOM */
-const imgPokemon = document.getElementById("img-pokemon")
-console.log(imgPokemon);
+const imgPokemon = document.getElementById("img-pokemon");
 
-const idPokemon = document.getElementById("id-pokemon")
-console.log(idPokemon);
+const idPokemon = document.getElementById("id-pokemon");
 
-const nombrePokemon = document.getElementById("nombre-pokemon")
-console.log(nombrePokemon);
+const nombrePokemon = document.getElementById("nombre-pokemon");
+
+const listaHabilidades = document.getElementById("lista-habilidades");
+
+const listaTipos = document.getElementById("lista-tipos");
+
+const formulario = document.getElementById("buscador-pokemon");
+console.log(formulario);
+
+/* Eventos */
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputPokemon = document.getElementById("busqueda-pokemon");
+  console.log(inputPokemon.value);
+
+  const nuevaBusqueda = urlPokemon + inputPokemon.value;
+
+  console.log(urlPokemon);
+
+  obtenerPokemon(nuevaBusqueda);
+});
 
 /* Funciones */
-async function obtenerPokemon(url){
-    const respuesta = await fetch(url)
-    const datos = await respuesta.json()
- 
-    const pokemon = {
-        nombre: datos.forms[0].name,
-        habilidad: datos.abilities,
-        id: datos.id,
-        tipo: datos.types,
-        imagen: datos.sprites.other["official-artwork"].front_default,
+async function obtenerPokemon(url) {
+
+    try{
+        const respuesta = await fetch(url);
+        const datos = await respuesta.json();
+      
+        const pokemon = {
+          nombre: datos.forms[0].name,
+          habilidades: datos.abilities,
+          id: datos.id,
+          tipos: datos.types,
+          imagen: datos.sprites.other["official-artwork"].front_default,
+        };
+      
+        // imagen, nombre y ID
+        imgPokemon.src = pokemon.imagen;
+        idPokemon.textContent = `ID: ${pokemon.id}`;
+        nombrePokemon.textContent = pokemon.nombre;
+      
+        // Habilidades
+      
+        let template = ``;
+      
+        for (let i = 0; i < pokemon.habilidades.length; i++) {
+          const nombreHabilidad = pokemon.habilidades[i].ability.name;
+      
+          template += `<li class="list-group-item">${nombreHabilidad}</li>`;
+        }
+      
+        listaHabilidades.innerHTML = template;
+      
+        // Tipos
+        let templateTipos = ``;
+      
+        pokemon.tipos.forEach((tipo) => {
+          const nombreTipo = tipo.type.name;
+      
+          templateTipos += `<li class="list-group-item">${nombreTipo}</li>`;
+        });
+      
+        listaTipos.innerHTML = templateTipos;
+
+        
+    }catch(e){
+        alert("pokemon no valido")
+
     }
-
-    imgPokemon.src = pokemon.imagen;
-    idPokemon.textContent = `ID: ${pokemon.id}`;
-    nombrePokemon.textContent = pokemon.nombre;
-
 
 }
 
-obtenerPokemon(urlPokemon)
+
