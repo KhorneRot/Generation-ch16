b33bd156c6805dffef9ad32a3754aad4d6fffd37
KhorneRot <ale_rm96@outlook.com>
2022-08-16 18:23:26 -0500

    Java

---
+++ 04-Java/Prueba1/src/main/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //String miVariable = "Hola mundo Java";
+        String nombre = "Alejandro Ramírez";
+        System.out.println("saludos desde Java a: " + nombre);
+    }
+}
---
+++ 04-Java/ejemploJDK/Hola.java
@@ -0,0 +1,15 @@
+public class Hola {
+//esto es una clase del jdk de java
+
+
+
+        public static void main(String[] args){
+        //es la declaración del método main
+
+                //String miVariable = "Hola Mundo saludo a: ";
+		String miNombre ="Alejandro Ramirez";
+                System.out.println(" Hola saludos a: " + miNombre);
+
+        }
+
+}
