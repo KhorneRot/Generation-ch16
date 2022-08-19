7b723ef0f93eb0c3b61086dd7e04c3461023a874
KhorneRot <ale_rm96@outlook.com>
2022-08-18 22:53:52 -0500

    Variables y operadores Java

---
+++ 4-Java/variables/src/OperadoresSumarios.java
@@ -0,0 +1,10 @@
+
+public class OperadoresSumarios {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = +i; // j = (1)*i -> -5
+        System.out.println("j = " + j);
+        int k = -1; //k = (-1) *1 -> 5
+        System.out.println("j = " + j);
+    }
+}
---
+++ 4-Java/variables/src/Operadoresincrementales.java
@@ -0,0 +1,28 @@
+
+public class Operadoresincrementales {
+    public static void main(String[] args) {
+ 
+        // pre incremento
+        int i = 1;
+        
+        int j = ++i; // i = i + 1
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        
+        // post incremento
+        i = 2;
+        j = i++;
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+
+        j=--i;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+
+        i=2;
+        j=i--;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        }
+    }
